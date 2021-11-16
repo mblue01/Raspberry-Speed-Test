2d3f368b197b63b7b57530eea3a65f8f53e58e58
mblue01 <mike.blue01@gmail.com>
2021-11-15 19:19:44 -0500

    Update pi-Ookla-Installer.sh

--- pi-Ookla-Installer.sh
+++ pi-Ookla-Installer.sh
@@ -1,19 +1,50 @@
 #INSTALL RASPBERRY PI SPEED TEST
+# Run this file as root
 
 # update system
-sudo apt-get update
-sudo apt-get upgrade
+apt-get update
+apt-get upgrade
 
 # install needed packages
-sudo apt install apt-transport-https gnupg1 dirmngr
+apt install apt-transport-https gnupg1 dirmngr
 
 # add signing key and add packagecloud.io repository 
 wget -q -O - https://packagecloud.io/ookla/speedtest-cli/gpgkey | sudo apt-key add -
 echo "deb https://packagecloud.io/ookla/speedtest-cli/debian/ $(lsb_release -cs) main" | sudo tee  /etc/apt/sources.list.d/speedtest.list
 
-# grab updated list from new repo and install speedtest from packagecloud.io repo
-sudo apt update
-sudo apt install speedtest
+# install influxdb repo
+wget -qO- https://repos.influxdata.com/influxdb.key | sudo apt-key add -
+echo "deb https://repos.influxdata.com/debian $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/influxdb.list
+
+
+# grab updated list from new repos and install speedtest from packagecloud.io repo
+apt update
+apt install speedtest
+
+# install influxdb from influx repo
+apt install influxdb
+systemctl unmask influxdb
+systemctl enable influxdb
+systemctl start influxdb
+
 
 ### run speedtest
 speedtest
+
+### run & configure influx
+influx -username admin -password <password>
+
+# use the following influx commands to setup database
+
+# CREATE DATABASE internetspeed
+#CREATE USER "speedmonitor" WITH PASSWORD 'myPassword'
+#GRANT ALL ON "internetspeed" to "speedmonitor"
+#quit
+
+#apt install python3-influxdb
+
+
+
+
+
+
